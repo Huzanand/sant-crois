0c4945a496fff192b9ece39d9c4ff6422fd82a61
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-06-18 18:17:32 +0200

    add new lang german

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,11 +4,13 @@ import enTranslations from '../locales/English.json';
 import ukTranslations from '../locales/Ukrainian.json';
 import ruTranslations from '../locales/Russian.json';
 import frTranslations from '../locales/French.json';
+import deTranslations from '../locales/German.json'
 
 i18n.use(initReactI18next).init({
     resources: {
         English: { translation: enTranslations },
         Ukrainian: { translation: ukTranslations },
+        German: { translation: deTranslations },
         ru: { translation: ruTranslations },
         French: { translation: frTranslations },
     },
