8d5aa6aacbccb821362bd6665e57155dd55aedb5
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-06-23 00:16:47 +0200

    remove rus lang, name changing

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from '../locales/English.json';
 import ukTranslations from '../locales/Ukrainian.json';
-import ruTranslations from '../locales/Russian.json';
 import frTranslations from '../locales/French.json';
 import deTranslations from '../locales/German.json'
 
@@ -11,7 +10,6 @@ i18n.use(initReactI18next).init({
         English: { translation: enTranslations },
         Ukrainian: { translation: ukTranslations },
         German: { translation: deTranslations },
-        ru: { translation: ruTranslations },
         French: { translation: frTranslations },
     },
     lng: 'English',
