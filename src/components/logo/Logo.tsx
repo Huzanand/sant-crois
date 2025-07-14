4629483b27c1751ab004090e2072778a1c6b5ff1
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-07-14 10:23:48 +0200

    branading

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,23 +1,40 @@
-import styles from './logo.module.css'
-import Image from "next/image"
-import logo from "../../assets/Avatar.png";
+import styles from "./logo.module.css";
+import Image from "next/image";
+import logoTop from "../../assets/logoTop.svg";
+import logoBottom from "../../assets/logoBottom.svg";
 
-
-const Logo = ({footer = false}) => {
-
+const Logo = ({ footer = false }) => {
     return (
         <div className={styles.sideBar_headings}>
             <div>
-                <Image src={logo.src} alt="" width={40} height={40} />
+                {footer ? (
+                    <Image src={logoBottom.src} alt="" width={98} height={68} />
+                ) : (
+                    <Image src={logoTop.src} alt="" width={133} height={40} />
+                )}
             </div>
-            <div>
-                <h6 className={footer ? `${styles.headlines_title} ${styles.colorFFF}` : styles.headlines_title}>French Book</h6>
-                <p className={footer ? `${styles.headlines_subtitle} ${styles.colorFFF}` : styles.headlines_subtitle}>
-                    Let`s study!
+            {/* <div>
+                <h6
+                    className={
+                        footer
+                            ? `${styles.headlines_title} ${styles.colorFFF}`
+                            : styles.headlines_title
+                    }
+                >
+                    Sant Crois
+                </h6>
+                <p
+                    className={
+                        footer
+                            ? `${styles.headlines_subtitle} ${styles.colorFFF}`
+                            : styles.headlines_subtitle
+                    }
+                >
+                    Let’s study!
                 </p>
-            </div>
+            </div> */}
         </div>
-    )
-}
+    );
+};
 
-export default Logo
+export default Logo;
