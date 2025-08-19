8ac871f4c31a4cf777e8651f58f4a510b25820d1
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-08-19 18:10:57 +0200

    add navigation to home page for logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,37 +2,19 @@ import styles from "./logo.module.css";
 import Image from "next/image";
 import logoTop from "../../assets/logoTop.svg";
 import logoBottom from "../../assets/logoBottom.svg";
+import { useRouter } from "next/navigation";
 
 const Logo = ({ footer = false }) => {
+    const router = useRouter();
     return (
         <div className={styles.sideBar_headings}>
-            <div>
+            <div onClick={() => router.push("/")}>
                 {footer ? (
                     <Image src={logoBottom.src} alt="" width={98} height={68} />
                 ) : (
                     <Image src={logoTop.src} alt="" width={133} height={40} />
                 )}
             </div>
-            {/* <div>
-                <h6
-                    className={
-                        footer
-                            ? `${styles.headlines_title} ${styles.colorFFF}`
-                            : styles.headlines_title
-                    }
-                >
-                    Sant Crois
-                </h6>
-                <p
-                    className={
-                        footer
-                            ? `${styles.headlines_subtitle} ${styles.colorFFF}`
-                            : styles.headlines_subtitle
-                    }
-                >
-                    Let’s study!
-                </p>
-            </div> */}
         </div>
     );
 };
