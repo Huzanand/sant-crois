d0cc6688eaa575001a4b99ac986d867fd95ee3b2
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-08-27 22:17:24 +0200

    modify for server- and client-side translation

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,24 +1,38 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import enTranslations from '../locales/English.json';
-import ukTranslations from '../locales/Ukrainian.json';
-import frTranslations from '../locales/French.json';
-import deTranslations from '../locales/German.json'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslations from "../locales/English.json";
+import ukTranslations from "../locales/Ukrainian.json";
+import frTranslations from "../locales/French.json";
+import deTranslations from "../locales/German.json";
 
-i18n.use(initReactI18next).init({
-    resources: {
-        English: { translation: enTranslations },
-        Ukrainian: { translation: ukTranslations },
-        German: { translation: deTranslations },
-        French: { translation: frTranslations },
-    },
-    lng: 'English',
-    fallbackLng: 'English',
-    interpolation: { escapeValue: false },
-    react: {
-        bindI18n: 'languageChanged',
-        useSuspense: false,
-    }
-});
+const resources = {
+    English: { translation: enTranslations },
+    Ukrainian: { translation: ukTranslations },
+    French: { translation: frTranslations },
+    German: { translation: deTranslations },
+};
+
+if (typeof window !== "undefined" && !i18n.isInitialized) {
+    i18n.use(initReactI18next).init({
+        resources,
+        lng: "English",
+        fallbackLng: "English",
+        interpolation: { escapeValue: false },
+        react: { useSuspense: false },
+    });
+}
+
+export async function initI18n(locale: string) {
+    const i18nextServer = (await import("i18next")).default;
+
+    await i18nextServer.init({
+        resources,
+        lng: locale,
+        fallbackLng: "English",
+        interpolation: { escapeValue: false },
+    });
+
+    return i18nextServer;
+}
 
 export default i18n;
