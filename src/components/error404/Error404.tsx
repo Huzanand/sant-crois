80868916b648df2d014e4d7f73de3702bb72dc48
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-08-31 00:39:09 +0200

    add translation + separated content for different pages

--- src/components/error404/Error404.tsx
+++ src/components/error404/Error404.tsx
@@ -2,8 +2,14 @@ import styles from "./error404.module.css";
 import Image from "next/image";
 import maskot from "../../assets/maskot_not_found.png";
 import { useOwnStore } from "@/store/storeProvider";
+import { useLanguageSync } from "@/utils/useLanguage";
+import Link from "next/link";
 
-const Error404 = () => {
+interface errorProps {
+    page: "home" | "lesson" | "check";
+}
+
+const Error404: React.FC<errorProps> = ({ page }) => {
     const {
         fetchLessons,
         selectedLearningLanguage,
@@ -14,39 +20,52 @@ const Error404 = () => {
         resetSize,
     } = useOwnStore((state) => state);
 
+    const { t } = useLanguageSync();
+
     return (
         <div className={styles.container}>
             <div className={styles.text_content}>
-                <h2 className="headlines-l">Я ничего не смог найти...</h2>
-                <p className="body-l">
-                    Вы можете попробовать другие фильтры. Или вернуться на
-                    главную и выбрать из списка
-                </p>
+                <h2 className="headlines-l">{t("maskot404.header")}</h2>
+                {page === "home" ? (
+                    <p className="body-l">{t("maskot404.text")}</p>
+                ) : undefined}
 
-                <button
-                    className={styles.btn}
-                    onClick={() => {
-                        fetchLessons(
-                            12,
-                            "all",
-                            "All",
-                            selectedLearningLanguage,
-                            [],
-                            [],
-                            [],
-                            [],
-                            0,
-                            "rating"
-                        );
-                        setOffset(0);
-                        onSelectChange("selectedLanguageLevel", "All");
-                        setActiveTypeOfLesson("all");
-                        clearFilters();
-                        resetSize();
-                    }}
-                >
-                    <span className="buttons-l">To Home Page</span>
-                </button>
+                {page === "home" ? (
+                    <button
+                        className={styles.btn}
+                        onClick={() => {
+                            fetchLessons(
+                                12,
+                                "all",
+                                "All",
+                                selectedLearningLanguage,
+                                [],
+                                [],
+                                [],
+                                [],
+                                0,
+                                "rating"
+                            );
+                            setOffset(0);
+                            onSelectChange("selectedLanguageLevel", "All");
+                            setActiveTypeOfLesson("all");
+                            clearFilters();
+                            resetSize();
+                        }}
+                    >
+                        <span className="buttons-l">
+                            {t("maskot404.resetFilters")}
+                        </span>
+                    </button>
+                ) : (
+                    <Link href={"/"}>
+                        <button className={styles.btn}>
+                            <span className="buttons-l">
+                                {t("maskot404.homeButton")}
+                            </span>
+                        </button>
+                    </Link>
+                )}
             </div>
 
             <Image src={maskot.src} width={272} height={282} alt="maskot" />
