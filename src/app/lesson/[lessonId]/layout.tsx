6aa3deff8a2027ba55222ff282de6345386a0f3c
Huzanand <112494195+Huzanand@users.noreply.github.com>
2025-10-01 17:10:59 +0200

    add loyout for modal

---
+++ src/app/lesson/[lessonId]/layout.tsx
@@ -0,0 +1,18 @@
+export default function LessonLayout({
+    children,
+    modal,
+}: {
+    children: React.ReactNode;
+    modal: React.ReactNode;
+}) {
+    return (
+        <div
+            style={{
+                position: "relative",
+            }}
+        >
+            {children}
+            {modal}
+        </div>
+    );
+}
