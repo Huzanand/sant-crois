97a37a94e5512ea3350f2b707845b58e94d1513c
Oleksii <huzanand@gmaol.com>
2026-04-06 10:09:25 +0200

    constructor content container

---
+++ src/containers/constructor/constructorContent/ConstructorContent.tsx
@@ -0,0 +1,100 @@
+import Divider from "@/components/divider/Divider"
+import ExersiceContainer from "../exersiceContainer/ExersiceContainer"
+import LessonInfo from "../lessonInfo/LessonInfo"
+import styles from "./constructorContent.module.css"
+import VideoExercise from "@/containers/constructor/videoExercise/VideoExercise"
+import AudioExercise from "../audioExercise/AudioExercise"
+import ReadingExercise from "../readingExercise/ReadingExercise"
+import FillInBlankExercise from "../fillInBlancExercise/FillInBlankExercise"
+import MultipleChoiceExercise from "../multipleChoiceExercise/MultipleChoiceExercise"
+import ChoiceExercise from "../choiceExercise/ChoiceExercise"
+import { useOwnStore } from "@/store/storeProvider"
+import { useEffect } from "react"
+import { cleanOrphanedFiles } from "@/store/fileStorage"
+import { ITaskData } from "@/models"
+
+const ConstructorContent = () => {
+    const draft = useOwnStore(state => state.draft)
+
+    useEffect(() => {
+        if (draft.id) {
+            cleanOrphanedFiles(draft.id);
+        }
+    }, []);
+
+
+    const renderTasks = (tasks: ITaskData[]) => {
+        if (!tasks) return null;
+
+        return (
+            <div>
+                {tasks.map((task) => {
+                    switch (task.taskType) {
+                        case "MEDIA_TASK":
+                            switch (task.content?.contentType) {
+                                case "TEXT":
+                                    return (
+                                        <ExersiceContainer title="Reading type" exerciseType="TEXT" id={task.taskId} key={task.taskId}>
+                                            <ReadingExercise />
+                                        </ExersiceContainer>
+                                    );
+                                case "VIDEO":
+                                    return (
+                                        <ExersiceContainer title="Video Exercise" exerciseType='VIDEO' id={task.taskId} key={task.taskId}>
+                                            <VideoExercise />
+                                        </ExersiceContainer>
+                                    );
+                                case "AUDIO":
+                                    return (
+                                        <ExersiceContainer title="Audio Exercise" exerciseType="AUDIO" id={task.taskId} key={task.taskId}>
+                                            <AudioExercise id={task.taskId} />
+                                        </ExersiceContainer>
+                                    );
+                                case "FILL_TEMPLATE":
+                                    return (
+                                        <ExersiceContainer title="Fill in blancs" exerciseType="FILL_TEMPLATE" id={task.taskId} key={task.taskId}>
+                                            <FillInBlankExercise />
+                                        </ExersiceContainer>
+                                    );
+                                case "CHOOSE_TEMPLATE":
+                                    return (
+                                        <ExersiceContainer title="Select from options" exerciseType="CHOOSE_TEMPLATE" id={task.taskId} key={task.taskId}>
+                                            <MultipleChoiceExercise />
+                                        </ExersiceContainer>
+                                    );
+                                default:
+                                    return null;
+                            }
+
+                        case "CHOOSE_ANSWER":
+                            return (
+                                <ExersiceContainer title="Multiple Choice" exerciseType="CHOOSE_ANSWER" id={task.taskId} key={task.taskId}>
+                                    <ChoiceExercise type="CHOOSE_ANSWER" id={task.taskId} />
+                                </ExersiceContainer>
+                            );
+                        case "TRUE_FALSE":
+                            return (
+                                <ExersiceContainer title="True/False Questions" exerciseType='TRUE_FALSE' id={task.taskId} key={task.taskId}>
+                                    <ChoiceExercise type='TRUE_FALSE' id={task.taskId} />
+                                </ExersiceContainer>
+                            );
+                        default:
+                            return null;
+                    }
+                })}
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <LessonInfo />
+
+            <Divider margin="2rem 0" />
+
+            {renderTasks(draft.tasks)}
+        </div>
+    )
+}
+
+export default ConstructorContent
