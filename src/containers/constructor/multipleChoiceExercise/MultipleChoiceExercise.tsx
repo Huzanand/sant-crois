a8318a3ff40675cd48228c36bff08e04f0fd22bd
Oleksii <huzanand@gmaol.com>
2026-04-06 10:33:40 +0200

    multiple choice exersice

---
+++ src/containers/constructor/multipleChoiceExercise/MultipleChoiceExercise.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import styles from './multipleChoiceExercise.module.css';
+import TiptapEditor from '@/components/common/editor/TiptapEditor';
+
+const MultipleChoiceExercise = ({ onUpdate, initialContent }: { onUpdate?: (json: any) => void, initialContent?: any }) => {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <p className='headlines-m b-500'>Text or sentence for the assignment</p>
+                <p className={`body-m ${styles.description}`}>Add text for work.</p>
+                <p className={`body-m ${styles.description} ${styles.addMargin}`}>
+                    You can highlight words that need clarification. Idioms, phrases, slang, etc.
+                </p>
+            </div>
+
+            <div className={styles.input_container}>
+                <div className={styles.input_container__inner}>
+                    <TiptapEditor
+                        editorMode='CHOOSE_TEMPLATE'
+                        initialContent={initialContent}
+                        onUpdate={(html: string, json: any) => onUpdate?.(json)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MultipleChoiceExercise
