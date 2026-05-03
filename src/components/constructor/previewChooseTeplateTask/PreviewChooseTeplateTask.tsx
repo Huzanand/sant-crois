034fbf0acbf261e449c73dbfabb82334fbaa9f8c
Oleksii <huzanand@gmaol.com>
2026-05-03 18:13:19 +0200

    created preview choose template task

---
+++ src/components/constructor/previewChooseTeplateTask/PreviewChooseTeplateTask.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import styles from "./previewChooseTeplateTask.module.css";
+import { FillInBlank } from "@/components/common/editor/extensions/FillInBlank";
+import { IDraftTaskData } from "@/models";
+import { useLanguageSync } from "@/utils/useLanguage";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { useOwnStore } from "@/store/storeProvider";
+import { MultipleChoice } from "@/components/common/editor/extensions/MultipleChoice";
+
+type propsTypes = {
+    taskData: IDraftTaskData;
+    index: number;
+    readonly?: boolean;
+};
+
+const PreviewChooseTeplateTask: React.FC<propsTypes> = ({ taskData, index, readonly }) => {
+    const { t } = useLanguageSync();
+    const { selectedInterfaceLanguage } = useOwnStore((state) => state);
+
+    const content = (taskData.content as { contentSource: JSON })
+        ?.contentSource;
+
+    const editor = useEditor({
+        editable: false,
+        content: content,
+        extensions: [
+            StarterKit,
+            MultipleChoice,
+        ],
+        editorProps: {
+            attributes: {
+                class: styles.tiptap_content,
+            },
+        },
+        immediatelyRender: false,
+    })
+
+    return (
+        <div>
+            <h2 className="headlines-m" style={{ margin: "1.5rem 0" }}>
+                {`${t("exercise")} ${index}`}
+            </h2>
+
+            <p style={{ margin: "0 0 1.25rem", color: "var(--Blue-B500)" }}>
+                {taskData.taskDescriptions[selectedInterfaceLanguage]}
+            </p>
+
+            <div className={styles.content}>
+                <EditorContent editor={editor} />
+            </div>
+        </div>
+    );
+};
+
+export default PreviewChooseTeplateTask;
