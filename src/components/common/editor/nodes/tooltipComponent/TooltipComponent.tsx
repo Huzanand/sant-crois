1ca44b4ef3468c7dfda7a3865639bbfbdee1c7b6
Oleksii <huzanand@gmaol.com>
2026-05-03 18:11:01 +0200

    add custom styles to node

--- src/components/common/editor/nodes/tooltipComponent/TooltipComponent.tsx
+++ src/components/common/editor/nodes/tooltipComponent/TooltipComponent.tsx
@@ -2,30 +2,46 @@
 
 import { NodeViewWrapper } from '@tiptap/react'
 import styles from './TooltipComponent.module.css'
+import Tooltip from '@/components/tooltip/Tooltip'
 
 declare module '@tiptap/core' {
-  interface Storage {
-    TEXT: {
-      onEdit: (pos: number, attrs: { label: string; info: string }) => void
+    interface Storage {
+        TEXT: {
+            onEdit: (pos: number, attrs: { label: string; info: string }) => void
+        }
     }
-  }
 }
 
 export const TooltipComponent = (props: any) => {
     const { node, getPos, editor } = props
     const { label, info } = node.attrs
 
-    const handleClick = () => {
+    const handleEditClick = (e: React.MouseEvent) => {
+        e.preventDefault()
         if (editor.isEditable) {
             (editor as any).storage.TEXT.onEdit(getPos(), { label, info })
         }
     }
 
     return (
-        <NodeViewWrapper className={styles.container}>
-            <button type="button" className={styles.button} onClick={handleClick}>
-                {label}
-            </button>
+        <NodeViewWrapper className={styles.container} as="span" style={{ display: 'inline-block', lineHeight: 'inherit'}}>
+            {editor.isEditable ? (
+                <button
+                    type="button"
+                    className={styles.button}
+                    onClick={handleEditClick}
+                >
+                    {label}
+                </button>
+            ) : (
+                <Tooltip
+                    title={label}
+                    content={info}
+                    underline
+                >
+                    {label}
+                </Tooltip>
+            )}
         </NodeViewWrapper>
     )
 }
