b6e18cd6c523a69b8f096282b99c227a2a797178
Oleksii Domkin <huzanand@gmail.com>
2026-07-26 14:11:16 +0200

    bug fix

--- src/components/error404/Error404.tsx
+++ src/components/error404/Error404.tsx
@@ -6,77 +6,72 @@ import { useLanguageSync } from "@/utils/useLanguage";
 import Link from "next/link";
 
 interface errorProps {
-    page: "home" | "lesson" | "check";
+  page: "home" | "lesson" | "check";
 }
 
 const Error404: React.FC<errorProps> = ({ page }) => {
-    const {
-        fetchLessons,
-        selectedLearningLanguage,
-        setOffset,
-        onSelectChange,
-        setActiveTypeOfLesson,
-        clearFilters,
-        resetSize,
-    } = useOwnStore((state) => state);
+  const {
+    fetchLessons,
+    selectedLearningLanguage,
+    setOffset,
+    onSelectChange,
+    setActiveTypeOfLesson,
+    clearFilters,
+    resetSize,
+  } = useOwnStore((state) => state);
 
-    const { t } = useLanguageSync();
+  const { t } = useLanguageSync();
 
-    return (
-        <div
-            className={styles.container}
-            style={{
-                justifyContent:
-                    page === "home" ? "space-between" : "space-around",
+  return (
+    <div
+      className={styles.container}
+      style={{
+        justifyContent: page === "home" ? "space-between" : "space-around",
+      }}
+    >
+      <div className={styles.text_content}>
+        <h2 className="headlines-l">{t("maskot404.header")}</h2>
+        {page === "home" ? (
+          <p className="body-l">{t("maskot404.text")}</p>
+        ) : undefined}
+
+        {page === "home" ? (
+          <button
+            className={styles.btn}
+            onClick={() => {
+              fetchLessons(
+                12,
+                "all",
+                "All",
+                selectedLearningLanguage,
+                [],
+                [],
+                [],
+                [],
+                "rating",
+                0,
+              );
+              setOffset(0);
+              onSelectChange("selectedLanguageLevel", "All");
+              setActiveTypeOfLesson("all");
+              clearFilters();
+              resetSize();
             }}
-        >
-            <div className={styles.text_content}>
-                <h2 className="headlines-l">{t("maskot404.header")}</h2>
-                {page === "home" ? (
-                    <p className="body-l">{t("maskot404.text")}</p>
-                ) : undefined}
+          >
+            <span className="buttons-l">{t("maskot404.resetFilters")}</span>
+          </button>
+        ) : (
+          <Link href={"/"}>
+            <button className={styles.btn}>
+              <span className="buttons-l">{t("maskot404.homeButton")}</span>
+            </button>
+          </Link>
+        )}
+      </div>
 
-                {page === "home" ? (
-                    <button
-                        className={styles.btn}
-                        onClick={() => {
-                            fetchLessons(
-                                12,
-                                "all",
-                                "All",
-                                selectedLearningLanguage,
-                                [],
-                                [],
-                                [],
-                                [],
-                                0,
-                                "rating"
-                            );
-                            setOffset(0);
-                            onSelectChange("selectedLanguageLevel", "All");
-                            setActiveTypeOfLesson("all");
-                            clearFilters();
-                            resetSize();
-                        }}
-                    >
-                        <span className="buttons-l">
-                            {t("maskot404.resetFilters")}
-                        </span>
-                    </button>
-                ) : (
-                    <Link href={"/"}>
-                        <button className={styles.btn}>
-                            <span className="buttons-l">
-                                {t("maskot404.homeButton")}
-                            </span>
-                        </button>
-                    </Link>
-                )}
-            </div>
-
-            <Image src={maskot.src} width={272} height={282} alt="maskot" />
-        </div>
-    );
+      <Image src={maskot.src} width={272} height={282} alt="maskot" />
+    </div>
+  );
 };
 
 export default Error404;
