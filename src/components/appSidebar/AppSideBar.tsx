4cc8c0ddf6d88f59cec14de1d734eec032efd2b8
Oleksii Domkin <huzanand@gmail.com>
2026-08-08 18:41:32 +0200

    refactor AppSideBar for improved readability and structure

--- src/components/appSidebar/AppSideBar.tsx
+++ src/components/appSidebar/AppSideBar.tsx
@@ -16,145 +16,135 @@ import TruncateText from "@/utils/TrankateText";
 import dynamic from "next/dynamic";
 
 const BurgerMenu = dynamic(() => import("../burgerMenu/BurgerMenu"), {
-    ssr: false,
+  ssr: false,
 });
 
 const AppSideBar = () => {
-    const {
-        activeTypeOfLesson,
-        sortingOptions,
-        selectedSorting,
-        onSelectChange,
-        homePageContentHeight,
-    } = useOwnStore((state) => state);
+  const {
+    activeTypeOfLesson,
+    sortingOptions,
+    selectedSorting,
+    onSelectChange,
+    homePageContentHeight,
+  } = useOwnStore((state) => state);
 
-    const [settingsIsOpen, setSettingsIsOpen] = useState<boolean>(false);
-    const isMobile = useMobile(1012);
-    const is425px = useMobile(425);
+  const [settingsIsOpen, setSettingsIsOpen] = useState<boolean>(false);
+  const isMobile = useMobile(1012);
+  const is425px = useMobile(425);
 
-    const { t } = useLanguageSync();
+  const { t } = useLanguageSync();
 
-    const sidebarRef = useRef<HTMLDivElement | null>(null);
-    const settingsRef = useRef<HTMLDivElement | null>(null);
-    const sortingRef = useRef<HTMLDivElement | null>(null);
-    const filtersRef = useRef<HTMLDivElement | null>(null);
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+  const settingsRef = useRef<HTMLDivElement | null>(null);
+  const sortingRef = useRef<HTMLDivElement | null>(null);
+  const filtersRef = useRef<HTMLDivElement | null>(null);
 
-    const [filtersIsOpen, setFiltersIsOpen] = useState(false);
-    const [filtersHeightCulced, setFiltersHeightCulced] = useState<number>(0);
+  const [filtersIsOpen, setFiltersIsOpen] = useState(false);
+  const [filtersHeightCulced, setFiltersHeightCulced] = useState<number>(0);
 
-    useEffect(() => {
-        const observer = new ResizeObserver(() => {
-            if (!sidebarRef.current || !filtersRef.current) return;
+  useEffect(() => {
+    const observer = new ResizeObserver(() => {
+      if (!sidebarRef.current || !filtersRef.current) return;
 
-            const sidebarHeight = sidebarRef.current.offsetHeight;
-            const contentHeight = homePageContentHeight;
+      const sidebarHeight = sidebarRef.current.offsetHeight;
+      const contentHeight = homePageContentHeight;
 
-            const spareHeight = Math.ceil(
-                Math.max(
-                    contentHeight -
-                        sidebarHeight +
-                        filtersRef.current?.offsetHeight || 0,
-                    500
-                )
-            );
+      const spareHeight = Math.ceil(
+        Math.max(
+          contentHeight - sidebarHeight + filtersRef.current.offsetHeight || 0,
+          500,
+        ),
+      );
 
-            setFiltersHeightCulced(spareHeight);
-        });
+      setFiltersHeightCulced(spareHeight);
+    });
 
-        if (sidebarRef.current) observer.observe(sidebarRef.current);
+    if (sidebarRef.current) observer.observe(sidebarRef.current);
 
-        return () => observer.disconnect();
-    }, [homePageContentHeight]);
+    return () => observer.disconnect();
+  }, [homePageContentHeight]);
 
-    return (
-        <div className={styles.sideBar} ref={sidebarRef}>
-            {!isMobile && (
-                <div className={styles.sideBar_main} ref={settingsRef}>
-                    <div className={styles.content__container}>
-                        <div className={styles.logo}>
-                            <Logo />
-                        </div>
+  return (
+    <div className={styles.sideBar} ref={sidebarRef}>
+      {!isMobile && (
+        <div className={styles.sideBar_main} ref={settingsRef}>
+          <div className={styles.content__container}>
+            <div className={styles.logo}>
+              <Logo />
+            </div>
 
-                        {!settingsIsOpen && (
-                            <div className={styles.content}>
-                                <TypeOfLesson />
+            {!settingsIsOpen && (
+              <div className={styles.content}>
+                <TypeOfLesson />
 
-                                <Divider />
+                <Divider />
 
-                                <div
-                                    className={styles.typeItem}
-                                    onClick={() => setSettingsIsOpen(true)}
-                                >
-                                    <SettingsIco />
-                                    <span className="buttons-l ln24">
-                                        {t("settings")}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {settingsIsOpen && (
-                            <div style={{ alignSelf: "stretch" }}>
-                                <div
-                                    className={styles.typeItem}
-                                    onClick={() => setSettingsIsOpen(false)}
-                                >
-                                    <div style={{ transform: "rotate(90deg)" }}>
-                                        <ArrowDownIco />
-                                    </div>
-                                    <span className="buttons-l ln24">
-                                        {t(activeTypeOfLesson)}
-                                    </span>
-                                </div>
-
-                                <Divider />
-
-                                <Settings />
-                            </div>
-                        )}
-                    </div>
+                <div
+                  className={styles.typeItem}
+                  onClick={() => setSettingsIsOpen(true)}
+                >
+                  <SettingsIco />
+                  <span className="buttons-l ln24">{t("settings")}</span>
                 </div>
+              </div>
             )}
 
-            {isMobile && <BurgerMenu mode={"home"} />}
-
-            <div className={styles.filters}>
-                <div className={styles.filters__item} ref={sortingRef}>
-                    <SettingsSelect
-                        mode={isMobile ? "modal" : "default"}
-                        options={useTranslatedOptions(
-                            sortingOptions,
-                            "sortingOptions"
-                        )}
-                        selectedOption={selectedSorting}
-                        selectedOptionLabel={
-                            is425px
-                                ? TruncateText(
-                                      t(`selectedSorting.${selectedSorting}`),
-                                      15
-                                  )
-                                : t(`selectedSorting.${selectedSorting}`)
-                        }
-                        onChangeSelect={onSelectChange}
-                        changeField={"selectedSorting"}
-                        ico={<SortingIco />}
-                        activeIcon={<SortingIco fill={"#fff"} />}
-                        shadow={isMobile ? false : true}
-                        border={isMobile ? true : false}
-                        isSorting
-                    />
+            {settingsIsOpen && (
+              <div style={{ alignSelf: "stretch" }}>
+                <div
+                  className={styles.typeItem}
+                  onClick={() => setSettingsIsOpen(false)}
+                >
+                  <div style={{ transform: "rotate(90deg)" }}>
+                    <ArrowDownIco />
+                  </div>
+                  <span className="buttons-l ln24">
+                    {t(activeTypeOfLesson)}
+                  </span>
                 </div>
 
-                <div className={styles.filters__item} ref={filtersRef}>
-                    <Filters
-                        height={filtersHeightCulced}
-                        isOpen={filtersIsOpen}
-                        setIsOpen={setFiltersIsOpen}
-                    />
-                </div>
-            </div>
+                <Divider />
+
+                <Settings />
+              </div>
+            )}
+          </div>
         </div>
-    );
+      )}
+
+      {isMobile && <BurgerMenu mode={"home"} />}
+
+      <div className={styles.filters}>
+        <div className={styles.filters__item} ref={sortingRef}>
+          <SettingsSelect
+            mode={isMobile ? "modal" : "default"}
+            options={useTranslatedOptions(sortingOptions, "sortingOptions")}
+            selectedOption={selectedSorting}
+            selectedOptionLabel={
+              is425px
+                ? TruncateText(t(`selectedSorting.${selectedSorting}`), 15)
+                : t(`selectedSorting.${selectedSorting}`)
+            }
+            onChangeSelect={onSelectChange}
+            changeField={"selectedSorting"}
+            ico={<SortingIco />}
+            activeIcon={<SortingIco fill={"#fff"} />}
+            shadow={isMobile ? false : true}
+            border={isMobile ? true : false}
+            isSorting
+          />
+        </div>
+
+        <div className={styles.filters__item} ref={filtersRef}>
+          <Filters
+            height={filtersHeightCulced}
+            isOpen={filtersIsOpen}
+            setIsOpen={setFiltersIsOpen}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppSideBar;
